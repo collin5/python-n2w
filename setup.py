974e3b30dad258595217e014eea54ab16d84a757
Collins Abitekaniza <abtcolns@gmail.com>
2017-08-22 07:09:43 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="n2w",
+    version="0.1.0",
+    
+    author="Collins Abitekaniza",
+    author_email="abtcolns@gmail.com",
+
+    packages=['app'],
+    scripts=['app/n2w']
+)
