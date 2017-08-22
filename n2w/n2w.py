729adc20a3904557cfad29c1a682ac6ca3a3d912
Collins Abitekaniza <abtcolns@gmail.com>
2017-08-22 11:28:59 +0300

    Refactor, avoid using `input` keyword as var name

--- n2w/n2w.py
+++ n2w/n2w.py
@@ -9,32 +9,32 @@ class N2w(Convert):
     def __init__(self):
         super(N2w, self).__init__()
 
-    def convert(self, input=0):
-        if input == 0:
+    def convert(self, number=0):
+        if number == 0:
             return "zero"
         else:
             # check for invalid int
             try:
-                int(input)
+                int(number)
             except ValueError:
                 return "Input not a valid number"
 
             # check if number bigger than quintillion
-            if (len(str(input)) >= (len(self.greater_names) * 4) - 1):
+            if (len(str(number)) >= (len(self.greater_names) * 4) - 1):
                 return "Number too big"
 
-            input, prefix = int(input), ""
-            if input < 0:
-                prefix, input = "negative", -input
+            number, prefix = int(number), ""
+            if number < 0:
+                prefix, number = "negative", -number
 
             result, pointer = "", 0
             while True:
-                if int(input) % 1000 != 0:
+                if int(number) % 1000 != 0:
                     result = "{} {} {}".format(self.less_than_athousand(
-                        input % 1000), self.greater_names.get(pointer - 1), result)
+                        number % 1000), self.greater_names.get(pointer - 1), result)
                 pointer += 1
-                input /= 1000
+                number /= 1000
 
-                if int(input) < 1:
+                if int(number) < 1:
                     break
             return "{} {}".format(prefix, result).strip()
