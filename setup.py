dd8a56ee7422f13521e1b29c3d20382cbe650ae2
Collins Abitekaniza <abtcolns@gmail.com>
2017-08-22 11:39:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,25 @@
 
 from distutils.core import setup
 
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
 setup(
     name="n2w",
     version="0.1.0",
-    
+    decription="Python library to convert numbers to words",
+    long_description="Python library to covnert numbers to words",
+    kewords='library numbers algorithim words convert',
     author="Collins Abitekaniza",
     author_email="abtcolns@gmail.com",
     url="https://github.com/collin5/python-n2w",
-    packages = ['n2w']
+    packages=['n2w'],
+    install_requires=[
+        'markdown'
+    ],
+    include_package_data=True,
+    zip_safe=False
 )
